e7078d2fb1ec1327af83b934468ab65abf11ac30
Agustin Cardozo <Agustin.cardozo001@gmail.com>
2024-01-24 00:58:52 -0300

    Comiit 4

--- project/config/urls.py
+++ project/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('nombre_de_la_aplicacion.urls')),
+    path('', include('proyect.urls')),
 ]
